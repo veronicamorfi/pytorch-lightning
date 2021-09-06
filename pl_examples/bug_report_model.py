c14d0114d692aae85a13803b8fbcee178904b223
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-06 17:11:07 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -8,20 +8,23 @@ from tests.helpers import RandomDataset, BoringModel
 
 
 class SimpleModel(BoringModel):
+    def __init__(self, fail=True):
+        super().__init__()
+        self.fail = fail
+
     def training_step(self, batch, batch_idx):
         out = super().training_step(batch, batch_idx)
-        if self.current_epoch == 0 and batch_idx == 1:
+        if self.fail and self.current_epoch == 0 and batch_idx == 1:
             raise RuntimeError("fault tolerant exception")
         return out
 
 
-def run():
+def run(simulate_failure):
     os.environ["PL_FAULT_TOLERANT_TRAINING"] = "1"
     train_data = DataLoader(RandomDataset(32, 64), batch_size=2, num_workers=2)
     val_data = DataLoader(RandomDataset(32, 64), batch_size=2, num_workers=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2, num_workers=2)
 
-    model = SimpleModel()
+    model = SimpleModel(fail=simulate_failure)
     model.training_epoch_end = None
     trainer = Trainer(
         default_root_dir=os.getcwd(),
@@ -30,14 +33,11 @@ def run():
         weights_summary=None,
         precision=16,
         gpus=2,
-        # resume_from_checkpoint=".pl_auto_save.ckpt",
+        resume_from_checkpoint=(".pl_auto_save.ckpt" if simulate_failure else None),
         plugins=DeepSpeedPlugin(),
     )
     trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
-
-    print(trainer.checkpoint_callback.best_model_path)
 
 
 if __name__ == "__main__":
-    run()
+    run(simulate_failure=True)
