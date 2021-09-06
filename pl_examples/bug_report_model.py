5ecf4927ef9cdf42e0472b64717fc08c1bffdef0
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-06 11:57:20 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -1,46 +1,10 @@
 import os
 
-import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 
-from pytorch_lightning import LightningModule, Trainer
-
-
-class RandomDataset(Dataset):
-    def __init__(self, size, length):
-        self.len = length
-        self.data = torch.randn(length, size)
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return self.len
-
-
-class BoringModel(LightningModule):
-    def __init__(self):
-        super().__init__()
-        self.layer = torch.nn.Linear(32, 2)
-
-    def forward(self, x):
-        return self.layer(x)
-
-    def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
-
-    def validation_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("valid_loss", loss)
-
-    def test_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("test_loss", loss)
-
-    def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
+from pytorch_lightning import Trainer
+from pytorch_lightning.plugins import DeepSpeedPlugin
+from tests.helpers import RandomDataset, BoringModel
 
 
 def run():
@@ -50,13 +14,16 @@ def run():
     test_data = DataLoader(RandomDataset(32, 64), batch_size=2, num_workers=2)
 
     model = BoringModel()
+
     trainer = Trainer(
         default_root_dir=os.getcwd(),
         num_sanity_val_steps=0,
         max_epochs=2,
         weights_summary=None,
+        precision=16,
+        gpus=1,
         # resume_from_checkpoint="/Users/adrian/repositories/pytorch-lightning/pl_examples/lightning_logs/version_43/checkpoints/epoch=2-step=95.ckpt",
-        plugins="deepspeed",
+        plugins=DeepSpeedPlugin(),
     )
     trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
     trainer.test(model, dataloaders=test_data)
