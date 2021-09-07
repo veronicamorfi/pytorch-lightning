df3bedf4dd7a9c2d8ac0ff06802f7bd811175f1e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-07 12:08:14 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -43,7 +43,7 @@ class BoringModel(LightningModule):
 
 
 def run(resume=False):
-    os.environ["FAULT_TOLERANT_TRAINING"] = "1"
+    os.environ["PL_FAULT_TOLERANT_TRAINING"] = "1"
     train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
     val_dataloader = DataLoader(RandomDataset(320, 64), batch_size=2)
 
