8af6ad578bf2e6dad69e4fc718a97413199c1064
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-07 13:24:47 +0200

    exhausted

--- pytorch_lightning/loops/fit_loop.py
+++ pytorch_lightning/loops/fit_loop.py
@@ -264,7 +264,7 @@ class FitLoop(Loop):
     def on_save_checkpoint(self) -> Dict:
         state_dict = super().on_save_checkpoint()
         # FIXME(@tchaton) Should pass has_completed=True when iterator is exhausted ?
-        state_dict["dataloader_state_dict"] = self.trainer.train_dataloader.state_dict(has_completed=False)
+        state_dict["dataloader_state_dict"] = self.trainer.train_dataloader.state_dict(has_completed=True)
         return state_dict
 
     def on_load_checkpoint(self, state_dict: Dict) -> None:
