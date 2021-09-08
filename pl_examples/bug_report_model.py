b11aed4d6224f2902e1314fa4b72b08d3c11f0a8
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-08 11:22:49 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -60,14 +60,14 @@ def run(resume=False):
     )
     trainer = Trainer(
         default_root_dir=os.getcwd(),
-        gpus=2,  # for hang, switch to GPU
+        num_processes=1,  # for hang, switch to GPU
         limit_train_batches=1,
         limit_val_batches=0,
         num_sanity_val_steps=0,
         accelerator="ddp",
         max_epochs=(1 if not resume else 2),
         weights_summary=None,
-        # callbacks=[checkpoint_callback] if not resume else [],
+        callbacks=[checkpoint_callback],
         # checkpoint_callback=(not resume),
         resume_from_checkpoint=("checkpoints/epoch=00.ckpt" if resume else None),
     )
