8a17e18f705fc78310d632464c201d10023a0885
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-09-22 14:57:22 +0200

    minimal

--- pl_examples/repro.py
+++ pl_examples/repro.py
@@ -1,9 +1,7 @@
 import os
 import torch
-from argparse import ArgumentParser
 from torch.utils.data import Dataset, DataLoader
 from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.plugins import DeepSpeedPlugin
 
 
@@ -36,32 +34,24 @@ class BoringModel(LightningModule):
         return torch.optim.SGD(self.layer.parameters(), lr=0.1)
 
 
-def run(resume=False):
+def run():
     train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
     val_data = DataLoader(RandomDataset(320, 64), batch_size=2)
 
     model = BoringModel()
-    # checkpoint_callback = ModelCheckpoint(
-    #     dirpath="checkpoints/",
-    #     filename="{epoch:02d}",
-    # )
+
     trainer = Trainer(
         default_root_dir=os.getcwd(),
-        gpus=[1, 2],
+        gpus=[1, 3],
         num_sanity_val_steps=0,
         precision=16,
         accelerator="ddp",
         max_epochs=2,
         plugins=[DeepSpeedPlugin(stage=2)],
         weights_summary=None,
-        # callbacks=[checkpoint_callback],
-        # resume_from_checkpoint="checkpoints/epoch=01.ckpt" if resume else None,
     )
     trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--resume", action="store_true")
-    args = parser.parse_args()
-    run(resume=args.resume)
+    run()
